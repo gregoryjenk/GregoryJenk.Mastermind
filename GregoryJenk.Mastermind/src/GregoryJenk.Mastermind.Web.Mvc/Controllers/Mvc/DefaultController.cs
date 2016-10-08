a2e9c608432612626d7620c93877c330b40ee771
Gregory Jenkins <gregoryjenk@gmail.com>
2016-10-08 14:22:24 +0100

    Adding authentication actions to the default controller.

--- GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/Controllers/Mvc/DefaultController.cs
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/Controllers/Mvc/DefaultController.cs
@@ -1,21 +1,49 @@
-﻿using System;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace GregoryJenk.Mastermind.Web.Mvc.Controllers
 {
     public class DefaultController : Controller
     {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        [HttpGet, Route("/external")]
+        public IActionResult External(string authenticationScheme)
+        {
+            var authenticationProperties = new AuthenticationProperties()
+            {
+                RedirectUri = "/"
+            };
+
+            return new ChallengeResult(authenticationScheme, authenticationProperties);
+        }
+
         [Route("/")]
         public IActionResult Index()
         {
             return View();
         }
 
-        [Route("/error")]
-        public IActionResult Error()
+        [HttpGet, Route("/login")]
+        public IActionResult Login()
         {
             return View();
+        }
+
+        [Authorize, HttpGet, Route("/logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.Authentication.SignOutAsync("Cookies");
+
+            return Redirect("/login");
         }
     }
 }
