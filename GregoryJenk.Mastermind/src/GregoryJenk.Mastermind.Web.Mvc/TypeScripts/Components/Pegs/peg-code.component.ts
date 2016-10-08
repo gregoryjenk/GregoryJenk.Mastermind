5c8b19f5379dd3bfe6271d6bf0ca88b2c9abaf42
Gregory Jenkins <gregoryjenk@gmail.com>
2016-10-08 22:27:37 +0100

    Adding code peg elements.

--- GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Components/Pegs/peg-code.component.ts
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Components/Pegs/peg-code.component.ts
@@ -10,4 +10,33 @@ export class PegCodeComponent {
     constructor() {
 
     }
+
+    private readColourClass(colour: Colour): string {
+        let colourClass: string;
+
+        switch (colour) {
+            case Colour.Blue:
+                colourClass = "peg-code__blue";
+                break;
+            case Colour.Green:
+                colourClass = "peg-code__green";
+                break;
+            case Colour.Orange:
+                colourClass = "peg-code__orange";
+                break;
+            case Colour.Purple:
+                colourClass = "peg-code__purple";
+                break;
+            case Colour.Red:
+                colourClass = "peg-code__red";
+                break;
+            case Colour.Yellow:
+                colourClass = "peg-code__yellow";
+                break;
+            default:
+                throw new TypeError("No matching colour found");
+        }
+
+        return colourClass;
+    }
 }
