66a1f0d4daba69217fe4c94f48225c767765709d
Gregory Jenkins <gregoryjenk@gmail.com>
2016-11-02 22:02:03 +0000

    Starting to implement the notification service.

---
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Services/Notifications/notification.service.ts
@@ -0,0 +1,7 @@
+﻿import { Injectable } from "@angular/core";
+//import { Notification } from "../../Models/Notifications/notification.model";
+
+@Injectable()
+export class NotificationService {
+
+}
