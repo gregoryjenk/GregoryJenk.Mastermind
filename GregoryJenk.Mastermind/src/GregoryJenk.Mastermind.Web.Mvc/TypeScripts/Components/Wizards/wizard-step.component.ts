e135e372c27a9917de14ae5e00af49e90d61100d
Gregory Jenkins <gregoryjenk@gmail.com>
2016-11-12 20:17:42 +0000

    Adding wizard components and templates.

---
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Components/Wizards/wizard-step.component.ts
@@ -0,0 +1,42 @@
+﻿import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { WizardComponent } from "./wizard.component";
+
+@Component({
+    selector: "wizard-step",
+    host: {
+        "[style.display]": "current ? 'block' : 'none'",
+    },
+    templateUrl: "/app/templates/wizards/wizard-step.component.html",
+})
+export class WizardStepComponent {
+    @Input() name: string = "";
+    @Input() backVisible: boolean = true;
+    @Input() iconClass: string;
+    @Input() nextEnable: boolean = true;
+    @Input() nextText: string = "Next";
+    @Input() nextVisible: boolean = true;
+    @Output() nextClicked = new EventEmitter();
+    @Output() stepEvent = new EventEmitter();
+
+    private step: number;
+
+    public current: boolean = false;
+
+    constructor(private parent: WizardComponent) {
+
+    }
+
+    ngOnInit() {
+        this.step = this.parent.addStep(this);
+
+        this.current = (this.step === this.parent.step);
+
+        this.parent.stepChange.subscribe((step: number) => {
+            this.current = (this.step === step);
+
+            if (this.current) {
+                this.stepEvent.emit();
+            }
+        });
+    }
+}
---
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Components/Wizards/wizard.component.ts
@@ -0,0 +1,74 @@
+﻿import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { WizardStepComponent } from "./wizard-step.component";
+
+@Component({
+    selector: "wizard",
+    templateUrl: "/app/templates/wizards/wizard.component.html",
+})
+export class WizardComponent {
+    @Input() completeText: string = "Done";
+    @Input() step: number = 1;
+    @Output() completed = new EventEmitter();
+    @Output() stepChange = new EventEmitter();
+
+    private nextText: string = "Next";
+    private steps: WizardStepComponent[] = [];
+
+    public addStep(wizardStep: WizardStepComponent) {
+        const addedStepNumber = this.steps.length + 1;
+
+        this.steps.push(wizardStep);
+
+        return addedStepNumber;
+    }
+
+    public backStepVisible() {
+        var currentWizardStep: WizardStepComponent = this.steps.find(x => x.current == true);
+
+        return !(this.step === 1) && (currentWizardStep.backVisible == true);
+    }
+
+    public backStep() {
+        this.stepChange.emit(this.step - 1);
+
+        this.nextStepText();
+    }
+
+    public nextStepEnable() {
+        var currentWizardStep: WizardStepComponent = this.steps.find(x => x.current == true);
+
+        return (currentWizardStep.nextEnable == true);
+    }
+
+    public nextStepText() {
+        var currentWizardStep: WizardStepComponent = this.steps.find(x => x.current == true);
+
+        this.nextText = currentWizardStep.nextText;
+    }
+
+    public nextStepVisible() {
+        var currentWizardStep: WizardStepComponent = this.steps.find(x => x.current == true);
+
+        return !(this.step === this.steps.length) && (currentWizardStep.nextVisible == true);
+    }
+
+    public nextStep() {
+        if (this.nextStepEnable()) {
+            var currentWizardStep: WizardStepComponent = this.steps.find(x => x.current == true);
+
+            currentWizardStep.nextClicked.emit();
+
+            this.stepChange.emit(this.step + 1);
+
+            this.nextStepText();
+        }
+    }
+
+    public completeVisible() {
+        return this.step === this.steps.length;
+    }
+
+    public complete() {
+        this.completed.emit()
+    }
+}
