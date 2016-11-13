80403bca7aba5187c9cdaa297bfebf48c1fff7ed
Gregory Jenkins <gregoryjenk@gmail.com>
2016-11-13 22:32:01 +0000

    Moving game login into game component elements.

--- GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Components/Games/game.component.ts
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Components/Games/game.component.ts
@@ -7,7 +7,36 @@ import { PegCode } from "../../Models/Pegs/peg-code.model";
     templateUrl: "/app/templates/games/game.component.html"
 })
 export class GameComponent {
-    constructor() {
+    //******************************************************************************
+    //TODO: Guess feedback rule where duplicates colours and wrong places.
+    //TODO: Point based scoring system.
+    //TODO: Five guess algorithm.
 
+    private _colours: PegCode[] = [];
+    private _currentGame: Game;
+    private _playedGames: Game[] = [];
+
+    constructor() {
+        this.configureColours();
     }
+
+    private createGame() {
+        this._currentGame = new Game();
+    }
+
+    private startGame() {
+        //this._currentGame.start();
+    }
+
+    private configureColours() {
+        //Cannot loop through const enum, so have to list them out.
+        this._colours.push(new PegCode(PegCodeColour.Blue));
+        this._colours.push(new PegCode(PegCodeColour.Green));
+        this._colours.push(new PegCode(PegCodeColour.Orange));
+        this._colours.push(new PegCode(PegCodeColour.Purple));
+        this._colours.push(new PegCode(PegCodeColour.Red));
+        this._colours.push(new PegCode(PegCodeColour.Yellow));
+    }
+
+    //******************************************************************************
 }
--- GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Components/app.component.ts
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Components/app.component.ts
@@ -1,42 +1,11 @@
 ﻿import { Component } from "@angular/core";
-import { Game } from "../Models/Games/game.model";
-import { PegCode } from "../Models/Pegs/peg-code.model";
 
 @Component({
     selector: ".app-component",
     templateUrl: "/app/templates/app.component.html"
 })
 export class AppComponent {
-    //******************************************************************************
-    //TODO: Guess feedback rule where duplicates colours and wrong places.
-    //TODO: Point based scoring system.
-    //TODO: Five guess algorithm.
-
-    private _colours: PegCode[] = [];
-    private _currentGame: Game;
-    private _playedGames: Game[] = [];
-
     constructor() {
-        this.configureColours();
+        
     }
-
-    private createGame() {
-        this._currentGame = new Game();
-    }
-
-    private startGame() {
-        //this._currentGame.start();
-    }
-
-    private configureColours() {
-        //Cannot loop through const enum, so have to list them out.
-        this._colours.push(new PegCode(PegCodeColour.Blue));
-        this._colours.push(new PegCode(PegCodeColour.Green));
-        this._colours.push(new PegCode(PegCodeColour.Orange));
-        this._colours.push(new PegCode(PegCodeColour.Purple));
-        this._colours.push(new PegCode(PegCodeColour.Red));
-        this._colours.push(new PegCode(PegCodeColour.Yellow));
-    }
-
-    //******************************************************************************
 }
