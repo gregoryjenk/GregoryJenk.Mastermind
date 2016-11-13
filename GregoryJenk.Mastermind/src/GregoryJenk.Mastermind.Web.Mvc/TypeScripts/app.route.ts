f93e788abf6645bfe9ec319396de606098c04cca
Gregory Jenkins <gregoryjenk@gmail.com>
2016-11-13 20:24:07 +0000

    Adding Angular 2 route configuration.

---
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/app.route.ts
@@ -0,0 +1,29 @@
+﻿import { Routes, RouterModule } from "@angular/router";
+//import { GameComponent } from "./Components/Games/game.component";
+
+const appRoutes: Routes = [
+    //{
+    //    component: DashboardComponent,
+    //    path: ""
+    //    //pathMatch: "full",
+    //    //redirectTo: "dashboard"
+    //},
+    //{
+    //    component: DashboardComponent,
+    //    path: "dashboard"
+    //},
+    //{
+    //    component: GameComponent,
+    //    path: "play"
+    //},
+    //{
+    //    component: PageNotFoundComponent,
+    //    path: "**"
+    //}
+];
+
+export const appRoutingProviders: any[] = [
+
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
