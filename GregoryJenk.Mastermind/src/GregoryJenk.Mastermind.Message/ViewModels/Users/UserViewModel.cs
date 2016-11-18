a4b9c8e56c0df07947df06cf73bf176555a0981e
Gregory Jenkins <gregoryjenk@gmail.com>
2016-11-18 20:56:35 +0000

    Adding the user view model and extension.

---
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Message/ViewModels/Users/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace GregoryJenk.Mastermind.Message.ViewModels.Users
+{
+    public class UserViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/Extensions/Users/UserViewModelExtension.cs
@@ -0,0 +1,21 @@
+﻿using GregoryJenk.Mastermind.Message.ViewModels.Users;
+using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace GregoryJenk.Mastermind.Web.Mvc.Extensions.Users
+{
+    public static class UserViewModelExtension
+    {
+        /// <summary>
+        /// Converts claims from within the current context into the user view model.
+        /// </summary>
+        /// <param name="userViewModel"></param>
+        /// <param name="claimsPrincipal"></param>
+        public static void Convert(this UserViewModel userViewModel, ClaimsPrincipal claimsPrincipal)
+        {
+            userViewModel.Name = claimsPrincipal.Claims.Single(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name").Value;
+            userViewModel.Email = claimsPrincipal.Claims.Single(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
+        }
+    }
+}
