61a525e4cecb02f3c064d244de184fa5c1b2d8d2
Gregory Jenkins <gregoryjenk@gmail.com>
2016-11-19 10:32:02 +0000

    Adding custom Angular 2 error handler.

---
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/app.error.ts
@@ -0,0 +1,13 @@
+﻿import { ErrorHandler } from "@angular/core";
+
+export class AppErrorHandler extends ErrorHandler {
+    //TODO: Add proper constructor(rethrowError?: boolean) in error handling.
+    constructor() {
+        super(false);
+    }
+
+    handleError(error: any): void {
+        //TODO: Check for production mode in error handling.
+        window.location.replace("/error");
+    }
+}
