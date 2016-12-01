4807e9696045e6b9047c592f46d0af6df7116327
Gregory Jenkins <gregoryjenk@gmail.com>
2016-12-01 21:05:38 +0000

    Adding a base view model with common properties and logic.

---
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Message/ViewModels/ViewModelBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace GregoryJenk.Mastermind.Message.ViewModels
+{
+    public abstract class ViewModelBase<TId>
+    {
+        public TId Id { get; set; }
+    }
+}
