33a021d6f83a11f472fdef0b2616fc334ee7f307
Gregory Jenkins <gregoryjenk@gmail.com>
2016-12-10 21:31:19 +0000

    Starting to add logic to the game API controller.

--- GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/Controllers/Api/Games/GameController.cs
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/Controllers/Api/Games/GameController.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Linq;
+﻿using GregoryJenk.Mastermind.Message.ViewModels.Games;
+using GregoryJenk.Mastermind.Message.ViewModels.Users;
+using GregoryJenk.Mastermind.Web.Mvc.Extensions.Users;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace GregoryJenk.Mastermind.Web.Mvc.Controllers.Api.Games
 {
+    [Authorize, Route("/api/game")]
     public class GameController : Controller
     {
-        
+        [HttpPost, Route("")]
+        public IActionResult Create([FromBody] GameViewModel game)
+        {
+            UserViewModel user = new UserViewModel();
+
+            user.Convert(User);
+
+            //TODO: Create game.
+
+            return Created(string.Format("api/game/{0}", game.Id), game);
+        }
     }
 }
