5a142ff6d4056745f620b24d1b4ae45891e416be
Gregory Jenkins <gregoryjenk@gmail.com>
2016-12-25 18:07:02 +0000

    Adding external user view model and service client interface.

---
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Message/ViewModels/Users/ExternalUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace GregoryJenk.Mastermind.Message.ViewModels.Users
+{
+    /// <summary>
+    /// No need to inherit from the base view model as this is used for external messaging.
+    /// </summary>
+    public class ExternalUserViewModel
+    {
+        public string Image { get; set; }
+    }
+}
---
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/ServiceClients/Users/IExternalUserServiceClient.cs
@@ -0,0 +1,11 @@
+﻿using GregoryJenk.Mastermind.Message.ViewModels.Users;
+using System;
+using System.Linq;
+
+namespace GregoryJenk.Mastermind.Web.Mvc.ServiceClients.Users
+{
+    public interface IExternalUserServiceClient : IReadOnlyServiceClient<ExternalUserViewModel, string>
+    {
+
+    }
+}
