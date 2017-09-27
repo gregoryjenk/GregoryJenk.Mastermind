88a3392ee41c6ab92842c7953045defa1bfd3ed9
Gregory Jenkins <gregoryjenk@gmail.com>
2017-09-27 21:42:55 +0100

    Updating the user secrets reference.

--- GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/Controllers/Mvc/DefaultController.cs
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/Controllers/Mvc/DefaultController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
