1e9658e016de3fcf101d9691ce262ce2a2b43c6d
Gregory Jenkins <gregoryjenk@gmail.com>
2017-10-16 22:09:42 +0100

    Updating the external user service client factory test.

--- GregoryJenk.Mastermind/test/GregoryJenk.Mastermind.Web.Mvc.Test/Factories/Users/ExternalUserServiceClientFactoryTest.cs
+++ GregoryJenk.Mastermind/test/GregoryJenk.Mastermind.Web.Mvc.Test/Factories/Users/ExternalUserServiceClientFactoryTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using GregoryJenk.Mastermind.Web.Mvc.Factories.Users;
+using GregoryJenk.Mastermind.Web.Mvc.ServiceClients.Users;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
 
@@ -8,8 +10,16 @@ namespace GregoryJenk.Mastermind.Web.Mvc.Test.Factories.Users
     public class ExternalUserServiceClientFactoryTest
     {
         [TestMethod]
-        public void IsTrueEqualToTrue()
+        public void CreateWithGoogleWillReturnGoogleUserServiceClient()
         {
+            //Arrange.
+            //var externalUserServiceClientFactory = new ExternalUserServiceClientFactory();
+
+            //Act.
+            //var googleUserServiceClient = externalUserServiceClientFactory.Create("google");
+
+            //Assert.
+            //Assert.IsInstanceOfType(googleUserServiceClient, typeof(GoogleUserServiceClient));
             Assert.AreEqual(true, true);
         }
     }
