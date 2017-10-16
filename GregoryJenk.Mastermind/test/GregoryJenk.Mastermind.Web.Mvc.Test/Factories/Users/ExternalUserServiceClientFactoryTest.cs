ffd6b23724579cb179fc3b6c57bf04282ff6238c
Gregory Jenkins <gregoryjenk@gmail.com>
2017-10-16 21:08:59 +0100

    Starting to implement testing for .NET Core.

---
+++ GregoryJenk.Mastermind/test/GregoryJenk.Mastermind.Web.Mvc.Test/Factories/Users/ExternalUserServiceClientFactoryTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace GregoryJenk.Mastermind.Web.Mvc.Test.Factories.Users
+{
+    [TestClass]
+    public class ExternalUserServiceClientFactoryTest
+    {
+        [TestMethod]
+        public void IsTrueEqualToTrue()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
