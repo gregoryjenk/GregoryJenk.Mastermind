04bbe8841c933e43147afe20b68ec551da4412ef
Gregory Jenkins <gregoryjenk@gmail.com>
2017-10-18 20:51:10 +0100

    Updating the external user service client factory unit test.

--- GregoryJenk.Mastermind/test/GregoryJenk.Mastermind.Web.Mvc.Test/Factories/Users/ExternalUserServiceClientFactoryTest.cs
+++ GregoryJenk.Mastermind/test/GregoryJenk.Mastermind.Web.Mvc.Test/Factories/Users/ExternalUserServiceClientFactoryTest.cs
@@ -1,26 +1,37 @@
 ﻿using GregoryJenk.Mastermind.Web.Mvc.Factories.Users;
+using GregoryJenk.Mastermind.Web.Mvc.Options.Services.Google;
 using GregoryJenk.Mastermind.Web.Mvc.ServiceClients.Users;
+using Microsoft.Extensions.Options;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Linq;
 
 namespace GregoryJenk.Mastermind.Web.Mvc.Test.Factories.Users
 {
-    [TestClass]
+    [TestClass, TestCategory("Unit")]
     public class ExternalUserServiceClientFactoryTest
     {
-        [TestMethod, TestCategory("Unit")]
+        [TestMethod]
         public void CreateWithGoogleWillReturnGoogleUserServiceClient()
         {
             //Arrange.
-            //var externalUserServiceClientFactory = new ExternalUserServiceClientFactory();
+            var googleServiceOptionMock = new Mock<IOptions<GoogleServiceOption>>();
+            googleServiceOptionMock.Setup(x => x.Value).Returns(new GoogleServiceOption()
+            {
+                BaseUrl = new Uri("http://foo.bar")
+            });
+
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            serviceProviderMock.Setup(x => x.GetService(typeof(IOptions<GoogleServiceOption>))).Returns(googleServiceOptionMock.Object);
+
+            var externalUserServiceClientFactory = new ExternalUserServiceClientFactory(serviceProviderMock.Object);
 
             //Act.
-            //var googleUserServiceClient = externalUserServiceClientFactory.Create("google");
+            var googleUserServiceClient = externalUserServiceClientFactory.Create("google");
 
             //Assert.
-            //Assert.IsInstanceOfType(googleUserServiceClient, typeof(GoogleUserServiceClient));
-            Assert.AreEqual(true, true);
+            Assert.IsInstanceOfType(googleUserServiceClient, typeof(GoogleUserServiceClient));
         }
     }
 }
