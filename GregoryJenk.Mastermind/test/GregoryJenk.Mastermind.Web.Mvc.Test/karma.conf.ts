03ae5f3e5a34eb8f82569dd473ee086f6c15979c
Gregory Jenkins <gregoryjenk@gmail.com>
2017-10-19 22:17:33 +0100

    #2 Implementing more for Karma and Jasmine tests.

--- GregoryJenk.Mastermind/test/GregoryJenk.Mastermind.Web.Mvc.Test/karma.conf.ts
+++ GregoryJenk.Mastermind/test/GregoryJenk.Mastermind.Web.Mvc.Test/karma.conf.ts
@@ -1,1 +1,29 @@
-﻿
+﻿export default config => {
+    config.set({
+        autoWatch: true,
+        browsers: [
+            "PhantomJS"
+        ],
+        files: [
+            {
+                pattern: "TypeScripts/**/*.ts"
+            }
+        ],
+        frameworks: [
+            "jasmine",
+            "karma-typescript"
+        ],
+        phantomjsLauncher: {
+            exitOnResourceError: true
+        },
+        preprocessors: {
+            "**/*.ts": [
+                "karma-typescript"
+            ]
+        },
+        reporters: [
+            "progress",
+            "karma-typescript"
+        ]
+    });
+}
