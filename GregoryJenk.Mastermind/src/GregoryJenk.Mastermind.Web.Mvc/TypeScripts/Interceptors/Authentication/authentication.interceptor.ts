9578e1b3bd814823d63a618c4e1b3155dcd53d83
Gregory Jenkins <gregoryjenk@gmail.com>
2017-12-25 15:18:59 +0000

    Updating the authentication interceptor. Also updating the NuGet and NPM package references.

--- GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Interceptors/Authentication/authentication.interceptor.ts
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Interceptors/Authentication/authentication.interceptor.ts
@@ -1,10 +1,43 @@
-﻿import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+﻿import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpResponse, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
+
+import "rxjs/add/operator/do";
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
     intercept(httpRequest: HttpRequest<any>, httpHandler: HttpHandler): Observable<HttpEvent<any>> {
-        return httpHandler.handle(httpRequest);
+        var authenticationHeaderScheme = this.readCookieValueByName("GregoryJenk.Mastermind.Web.Mvc.Controllers.Mvc.JwtToken.Scheme");
+        var authenticationHeaderValue = this.readCookieValueByName("GregoryJenk.Mastermind.Web.Mvc.Controllers.Mvc.JwtToken.Value");
+
+        const httpRequestCloned = httpRequest.clone({ headers: httpRequest.headers.set("Authorization", `${authenticationHeaderScheme} ${authenticationHeaderValue}`) });
+
+        return httpHandler.handle(httpRequestCloned)
+            .do((event: HttpEvent<any>) => {
+                if (event instanceof HttpResponse) {
+                    //Add any logic to successfull request responses here.
+                }
+            },
+            (error: any) => {
+                if (error instanceof HttpErrorResponse) {
+                    if (error.status === 401) {
+                        window.location.replace("/login");
+                    }
+                }
+            });
+    }
+
+    private readCookieValueByName(name: string): string {
+        const nameLength = name.length + 1;
+
+        return document.cookie
+            .split(";")
+            .map(callback => callback.trim())
+            .filter(cookie => {
+                return cookie.substring(0, nameLength) === `${name}=`;
+            })
+            .map(cookie => {
+                return decodeURIComponent(cookie.substring(nameLength));
+            })[0] || null;
     }
 }
