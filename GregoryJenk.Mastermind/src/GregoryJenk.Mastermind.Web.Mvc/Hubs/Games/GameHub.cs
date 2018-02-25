52bde293cba45840b10bdeef8f7defd3d463a844
Gregory Jenkins <gregoryjenk@gmail.com>
2018-02-25 18:01:02 +0000

    #13 Starting to implement SignalR for ASP.NET Core.

---
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Web.Mvc/Hubs/Games/GameHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GregoryJenk.Mastermind.Web.Mvc.Hubs.Games
+{
+    public class GameHub : Hub
+    {
+        public Task Send(string message)
+        {
+            return Clients.All.InvokeAsync("Send", message);
+        }
+    }
+}
