867239bb71a0b422f3409ecd9013a7b2c9f09387
Gregory Jenkins <gregoryjenk@gmail.com>
2018-05-27 23:44:04 +1000

    Updating the message NuGet package.

--- GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Cache.Redis/Caches/Users/UserCache.cs
+++ GregoryJenk.Mastermind/src/GregoryJenk.Mastermind.Cache.Redis/Caches/Users/UserCache.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using GregoryJenk.Mastermind.Message.ViewModels.Users;
+using System;
 using System.Linq;
 
 namespace GregoryJenk.Mastermind.Cache.Redis.Caches.Users
 {
-    public class UserCache //: BaseCache<UserViewModel>
+    public class UserCache : BaseCache<UserViewModel, string>, IUserCache
     {
 
     }
