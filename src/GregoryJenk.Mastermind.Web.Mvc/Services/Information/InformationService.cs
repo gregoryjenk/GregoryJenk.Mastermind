7962923e113c4f2c1d1d6a0a178a95a9eb9b9f51
Gregory Jenkins <gregoryjenk@gmail.com>
2020-09-22 19:42:33 +1000

    Adding information service to read assembly version.

---
+++ src/GregoryJenk.Mastermind.Web.Mvc/Services/Information/InformationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace GregoryJenk.Mastermind.Web.Mvc.Services.Information
+{
+    public static class InformationService
+    {
+        public static string ReadVersionByEntryAssembly()
+        {
+            var assemblyInformationalVersionAttribute = Assembly.GetEntryAssembly()
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+
+            return assemblyInformationalVersionAttribute.InformationalVersion;
+        }
+    }
+}
