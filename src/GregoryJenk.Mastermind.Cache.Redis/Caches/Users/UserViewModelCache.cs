d0a7a0159a434bc24bb4ae6211b890beef33220f
Gregory Jenkins <gregoryjenk@gmail.com>
2022-01-02 18:30:16 +1100

    Updating the Redis cache implementation.

---
+++ src/GregoryJenk.Mastermind.Cache.Redis/Caches/Users/UserViewModelCache.cs
@@ -0,0 +1,22 @@
+﻿using GregoryJenk.Mastermind.Message.Caches.Users;
+using GregoryJenk.Mastermind.Message.ViewModels.Users;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Linq;
+
+namespace GregoryJenk.Mastermind.Cache.Redis.Caches.Users
+{
+    public class UserViewModelCache : BaseViewModelCache<UserViewModel, Guid, string>, IUserViewModelCache
+    {
+        public UserViewModelCache(IDistributedCache distributedCache)
+            : base(distributedCache)
+        {
+
+        }
+
+        public string GenerateKey(UserViewModel userViewModel)
+        {
+            return $"user:{userViewModel.Scheme}:{userViewModel.Id}";
+        }
+    }
+}
