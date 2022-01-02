d290205d327b9f31b4771af7f3a9f0064d252104
Gregory Jenkins <gregoryjenk@gmail.com>
2022-01-02 11:34:10 +1100

    Updating the memory cache implementation.

---
+++ src/GregoryJenk.Mastermind.Cache.Memory/Caches/BaseViewModelCache.cs
@@ -0,0 +1,31 @@
+﻿using GregoryJenk.Mastermind.Message.ViewModels;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Linq;
+
+namespace GregoryJenk.Mastermind.Cache.Memory.Caches
+{
+    public abstract class BaseViewModelCache<VM, VmId, VmKey> where VM : BaseEntityViewModel<VmId>
+    {
+        public readonly IMemoryCache _memoryCache;
+
+        public BaseViewModelCache(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public void Upsert(VmKey key, VM viewModel)
+        {
+            _memoryCache.Set(key, viewModel);
+        }
+
+        public VM ReadByKey(VmKey key)
+        {
+            return _memoryCache.Get<VM>(key);
+        }
+
+        //Could have the abstract generate method here, so it could be used here
+        //and also implemented in the concrete class for the interface.
+        //public abstract VmKey GenerateKey(VM viewModel);
+    }
+}
---
+++ src/GregoryJenk.Mastermind.Cache.Memory/Caches/Users/UserViewModelCache.cs
@@ -0,0 +1,22 @@
+﻿using GregoryJenk.Mastermind.Message.Caches.Users;
+using GregoryJenk.Mastermind.Message.ViewModels.Users;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Linq;
+
+namespace GregoryJenk.Mastermind.Cache.Memory.Caches.Users
+{
+    public class UserViewModelCache : BaseViewModelCache<UserViewModel, Guid, string>, IUserViewModelCache
+    {
+        public UserViewModelCache(IMemoryCache memoryCache)
+            : base(memoryCache)
+        {
+
+        }
+
+        public string GenerateKey(UserViewModel userViewModel)
+        {
+            return $"user:{userViewModel.Scheme}:{userViewModel.Id}";
+        }
+    }
+}
