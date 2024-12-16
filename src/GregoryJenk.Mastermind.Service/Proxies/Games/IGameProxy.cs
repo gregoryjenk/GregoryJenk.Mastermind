d010c3e26cf39f4ff7e68b50dbc1ce4d3df2a99b
Gregory Jenkins <gregoryjenk@gmail.com>
2024-12-16 13:40:28 +1100

    Starting to refactor service clients into proxies.

---
+++ src/GregoryJenk.Mastermind.Service/Proxies/Games/IGameProxy.cs
@@ -0,0 +1,22 @@
+﻿using GregoryJenk.Mastermind.Message.Requests.Games;
+using GregoryJenk.Mastermind.Message.ViewModels.Games;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GregoryJenk.Mastermind.Service.Proxies.Games
+{
+    public interface IGameProxy
+    {
+        Task<GameViewModel> ReadByIdAsync(Guid id);
+
+        Task<GameViewModel> CreateAsync();
+
+        Task<IList<GameViewModel>> ReadByDecoderUserIdAsync();
+
+        Task<GameViewModel> CreateGuessAsync(GameCreateGuessRequest gameCreateGuessRequest);
+
+        Task<GameViewModel> UpdateStateAsync(GameUpdateStateRequest gameUpdateStateRequest);
+    }
+}
---
+++ src/GregoryJenk.Mastermind.Service/Proxies/ProxyOption.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace GregoryJenk.Mastermind.Service.Proxies
+{
+    public class ProxyOption
+    {
+        public Uri BaseUrl { get; set; }
+    }
+}
