4fd61d63618f604a560b46f10a74d2b91d43fc0a
Gregory Jenkins <gregoryjenk@gmail.com>
2025-01-03 19:33:14 +1100

    Updating user proxy.

---
+++ src/GregoryJenk.Mastermind.Service/Proxies/ProxyConstant.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace GregoryJenk.Mastermind.Service.Proxies
+{
+    public static class ProxyConstant
+    {
+        public const string DefaultName = "DefaultProxy";
+    }
+}
--- src/GregoryJenk.Mastermind.Service/Proxies/Users/UserProxy.cs
+++ src/GregoryJenk.Mastermind.Service/Proxies/Users/UserProxy.cs
@@ -1,15 +1,75 @@
 ﻿using GregoryJenk.Mastermind.Message.ViewModels.Users;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
 using System;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.Mime;
 using System.Threading.Tasks;
 
 namespace GregoryJenk.Mastermind.Service.Proxies.Users
 {
     public class UserProxy : IUserProxy
     {
+        private readonly Uri _baseUrl;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UserProxy(IOptions<ProxyOption> proxyOption, IHttpClientFactory httpClientFactory)
+        {
+            _baseUrl = proxyOption.Value.BaseUrl;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<UserViewModel> ReadAsync()
+        {
+            var httpClient = _httpClientFactory.CreateClient(ProxyConstant.DefaultName);
+
+            var httpRequestMessageUrl = new Uri(_baseUrl, "user");
+
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, httpRequestMessageUrl);
+
+            httpRequestMessage.Headers.Add(HeaderNames.Accept, MediaTypeNames.Application.Json);
+
+            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                return await httpResponseMessage.Content.ReadFromJsonAsync<UserViewModel>();
+            }
+            else
+            {
+                //When no user is found return null to be handled by the caller.
+                if (httpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+
+                throw new HttpRequestException();
+            }
+        }
+
         public async Task<UserViewModel> UpsertAsync()
         {
-            throw new NotImplementedException();
+            var httpClient = _httpClientFactory.CreateClient(ProxyConstant.DefaultName);
+
+            var httpRequestMessageUrl = new Uri(_baseUrl, "user");
+
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Put, httpRequestMessageUrl);
+
+            httpRequestMessage.Headers.Add(HeaderNames.Accept, MediaTypeNames.Application.Json);
+
+            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                return await httpResponseMessage.Content.ReadFromJsonAsync<UserViewModel>();
+            }
+            else
+            {
+                throw new HttpRequestException();
+            }
         }
     }
 }
