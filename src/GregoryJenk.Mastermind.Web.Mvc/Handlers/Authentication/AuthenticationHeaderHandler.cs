207db23c85b4dc5f7972f9b4bf780f45068c1f49
Gregory Jenkins <gregoryjenk@gmail.com>
2025-01-06 19:31:15 +1100

    Adding more authentication objects.

---
+++ src/GregoryJenk.Mastermind.Web.Mvc/Handlers/Authentication/AuthenticationHeaderHandler.cs
@@ -0,0 +1,32 @@
+﻿using GregoryJenk.Mastermind.Service.Strategies.Authentication;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GregoryJenk.Mastermind.Web.Mvc.Handlers.Authentication
+{
+    public class AuthenticationHeaderHandler : DelegatingHandler
+    {
+        private readonly IAuthenticationStoreStrategy _authenticationStoreStrategy;
+
+        public AuthenticationHeaderHandler(IAuthenticationStoreStrategy authenticationStoreStrategy)
+        {
+            _authenticationStoreStrategy = authenticationStoreStrategy;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage httpRequestMessage, CancellationToken cancellationToken)
+        {
+            if (httpRequestMessage.Headers.Authorization is null)
+            {
+                var authenticationStoreStrategyItem = _authenticationStoreStrategy.Get();
+
+                httpRequestMessage.Headers.Authorization = new AuthenticationHeaderValue(authenticationStoreStrategyItem.Scheme, authenticationStoreStrategyItem.Token);
+            }
+
+            return await base.SendAsync(httpRequestMessage, cancellationToken);
+        }
+    }
+}
--- src/GregoryJenk.Mastermind.Web.Mvc/Strategies/Authentication/AuthenticationTokenJwtBearerStrategyOption.cs
+++ src/GregoryJenk.Mastermind.Web.Mvc/Strategies/Authentication/AuthenticationTokenJwtBearerStrategyOption.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Linq;
 
-namespace GregoryJenk.Mastermind.Web.Mvc.Options.Authentication.Jwt
+namespace GregoryJenk.Mastermind.Web.Mvc.Strategies.Authentication
 {
-    public class JwtAuthenticationOption
+    public class AuthenticationTokenJwtBearerStrategyOption
     {
         public string IssuerSigningKey { get; set; }
+
         public string ValidAudience { get; set; }
     }
 }
