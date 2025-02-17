2ce399e2d7eea6fbc420a4e4bcc57aec18aa7c2d
Gregory Jenkins <gregoryjenk@gmail.com>
2025-02-17 15:53:46 +1100

    Deleting redundant TypeScript file.

--- src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/sec.main.ts
+++
@@ -1,7 +0,0 @@
-﻿import { enableProdMode } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { SecModule } from "./sec.module";
-
-enableProdMode();
-
-platformBrowserDynamic().bootstrapModule(SecModule);
