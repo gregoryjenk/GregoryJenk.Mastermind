2800a13f43ed79dcd5a1b600cda75f4ba694c448
Gregory Jenkins <gregoryjenk@gmail.com>
2025-02-28 20:26:49 +1100

    Adding TypeScript request objects.

---
+++ src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Requests/Games/game-create-guess.request.ts
@@ -0,0 +1,11 @@
+﻿import { GameGuessViewModel } from "../../ViewModels/Games/Guesses/game-guess.view-model";
+
+export class GameCreateGuessRequest {
+    public id: string;
+
+    public version: number;
+
+    public decoderUserId: string;
+
+    public guess: GameGuessViewModel;
+}
---
+++ src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Requests/Games/game-update-state.request.ts
@@ -0,0 +1,9 @@
+﻿export class GameUpdateStateRequest {
+    public id: string;
+
+    public version: number;
+
+    public state: GameState;
+
+    public decoderUserId: string;
+}
