4825f2d6ff1f0263b46bc6a88fa13970556482dc
Gregory Jenkins <gregoryjenk@gmail.com>
2025-03-12 02:44:11 +1100

    Creating game create component.

---
+++ src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Components/Games/game-create.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: "../../../wwwroot/app/templates/components/games/game-create.component.html"
+})
+export class GameCreateComponent {
+
+}
