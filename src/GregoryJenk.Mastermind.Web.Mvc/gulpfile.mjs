36d2648613c545f97c255eeffef75d49fe58201e
Gregory Jenkins <gregoryjenk@gmail.com>
2025-04-03 11:55:17 +1100

    Updating Gulp file.

--- src/GregoryJenk.Mastermind.Web.Mvc/gulpfile.mjs
+++ src/GregoryJenk.Mastermind.Web.Mvc/gulpfile.mjs
@@ -5,7 +5,7 @@ import { dest, series, src, watch } from "gulp";
 import cleanCss from "gulp-clean-css";
 import rename from "gulp-rename";
 import sassFactory from "gulp-sass";
-import sourcemaps from "gulp-sourcemaps";
+import sourceMaps from "gulp-sourcemaps";
 import * as sassCompiler from "sass";
 
 const paths = {
@@ -64,10 +64,10 @@ export function minimiseCss() {
     };
 
     return src("./wwwroot/app/css/**/*.css")
-        .pipe(sourcemaps.init())
+        .pipe(sourceMaps.init())
         .pipe(cleanCss())
         .pipe(rename(renameOptions))
-        .pipe(sourcemaps.write("."))
+        .pipe(sourceMaps.write("."))
         .pipe(dest("./wwwroot/app/css"));
 }
 
