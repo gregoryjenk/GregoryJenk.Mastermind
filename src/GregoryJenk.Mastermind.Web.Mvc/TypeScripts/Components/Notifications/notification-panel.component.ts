d4b0698c4e2e4350620b95c485d3c8e02f937795
Gregory Jenkins <gregoryjenk@gmail.com>
2025-04-16 13:38:21 +1000

    Renaming notification panel component.

---
+++ src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Components/Notifications/notification-panel.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from "@angular/core";
+import { NotificationService } from "../../Services/Notifications/notification.service";
+
+@Component({
+    selector: "notification-panel-component",
+    styleUrl: "../../../wwwroot/src/styles/components/notifications/notification-panel.component.scss",
+    templateUrl: "../../../wwwroot/app/templates/components/notifications/notification-panel.component.html"
+})
+export class NotificationPanelComponent {
+    constructor(private readonly notificationService: NotificationService) {
+
+    }
+}
