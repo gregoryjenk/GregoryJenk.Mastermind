774a8f423edb2d0ff9ce2ea15797e59461ba0d2a
Gregory Jenkins <gregoryjenk@gmail.com>
2025-04-18 02:42:27 +1000

    Creating game code peg function file.

---
+++ src/GregoryJenk.Mastermind.Web.Mvc/TypeScripts/Functions/Games/CodePegs/game-code-peg.function.ts
@@ -0,0 +1,48 @@
+﻿import { GameCodePegColour } from "../../../Models/Games/CodePegs/game-code-peg-colour";
+
+export function readColourName(gameCodePegColour: GameCodePegColour): string {
+    let colourName: string;
+
+    switch (gameCodePegColour) {
+        case GameCodePegColour.None: {
+            colourName = "None";
+
+            break;
+        }
+        case GameCodePegColour.Blue: {
+            colourName = "Blue";
+
+            break;
+        }
+        case GameCodePegColour.Green: {
+            colourName = "Green";
+
+            break;
+        }
+        case GameCodePegColour.Orange: {
+            colourName = "Orange";
+
+            break;
+        }
+        case GameCodePegColour.Purple: {
+            colourName = "Purple";
+
+            break;
+        }
+        case GameCodePegColour.Red: {
+            colourName = "Red";
+
+            break;
+        }
+        case GameCodePegColour.Yellow: {
+            colourName = "Yellow";
+
+            break;
+        }
+        default: {
+            throw new TypeError(`Colour not supported with ${gameCodePegColour}.`);
+        }
+    }
+
+    return colourName;
+}
